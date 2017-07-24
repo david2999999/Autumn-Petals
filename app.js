b43a3cd02aa26d63ad28d0c00fe86fb5bc25005c
David Jiang <djiang86@binghamton.edu>
2017-07-24 18:20:03 +0000

    Added HTTPS to reset password

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ var express         = require("express"),       // used to run the whole applica
 // console.log(process.env.DATABASEURL);
 mongoose.Promise = global.Promise;  // prevent a warning message
 mongoose.connect(process.env.DATABASEURL);// DATABASE HERE
-
 // mongoose.connect("mongodb://localhost/autumn-petals");
 
 //body parser allows to see the element value from the form
@@ -260,7 +259,7 @@ app.post('/forgot', function(req, res, next) {
         subject: 'Password Reset',
         text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
           'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-          'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+          'https://' + req.headers.host + '/reset/' + token + '\n\n' +
           'If you did not request this, please ignore this email and your password will remain unchanged.\n'
       };
       smtpTransport.sendMail(mailOptions, function(err) {
